5062d575cdba8a05cd4e678c72a41fbd0512eaa6
Artem Pavlov <pavlov.tema@gmail.com>
2021-11-16 21:24:25 +0500

    Initial Commit

---
+++ AnimatedDay/AnimatedDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedDayApp.swift
+//  AnimatedDay
+//
+//  Created by iMac on 16.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
